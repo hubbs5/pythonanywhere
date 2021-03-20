dc459521b0874cb826c1ede05755e2e366a99e8e
Christian <christiandhubbs@gmail.com>
2021-03-20 07:51:03 -0500

    adding logger

--- app/index.py
+++ app/index.py
@@ -1,10 +1,12 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import logging
 
 from app import app
 from pages import home, page1, page2
 
+logger = logging.getLogger("PA-Test")
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -21,7 +23,9 @@ def display_page(pathname):
     elif pathname == '/eth':
         return page2.layout
     else:
-        return home.layout
+        app.logger.warning("404 Error")
+        return "404", 
+        # return home.layout
 
 if __name__ == '__main__':
     app.run_server(debug=True)
